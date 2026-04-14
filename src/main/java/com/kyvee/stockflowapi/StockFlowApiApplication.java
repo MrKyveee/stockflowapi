6c6aa4049ce39c8fdd29f034c4ee92cdf8a50ccb
Kyvee <kyveedeveloper@gmail.com>
2026-04-14 20:35:59 -0300

    Initial commit

---
+++ src/main/java/com/kyvee/stockflowapi/StockFlowApiApplication.java
@@ -0,0 +1,13 @@
+package com.kyvee.stockflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockFlowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyvee/stockflowapi/StockFlowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyvee.stockflowapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockFlowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
